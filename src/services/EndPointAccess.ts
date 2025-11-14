0d2d515ca61f8010acb9ec728fdf37944f5920d0
mardiahnur <rahmimardiah92@gmail.com>
2025-11-14 11:24:29 +0800

    update EndPointAccess

--- src/services/EndPointAccess.ts
+++ src/services/EndPointAccess.ts
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
-  baseURL: 'https://api.open-meteo.com/v1/forecast',
-  timeout: 5000,
-});
-
 export async function getWeatherData() {
-  const response = await apiClient.get(
-    '?latitude=-6.2&longitude=106.8&hourly=temperature_2m'
+  const response = await axios.get(
+    'https://api.open-meteo.com/v1/forecast?latitude=-6.2&longitude=106.8&hourly=temperature_2m'
   );
   return response.data;
 }
